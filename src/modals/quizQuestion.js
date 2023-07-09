258f96584385bf0e5f7e92e5ce2ecbe98e98c540
joewrotehaikus <78707288+joewrotehaikus@users.noreply.github.com>
2023-07-09 17:36:06 -0400

    Add question details to response to quiz answer

--- src/modals/quizQuestion.js
+++ src/modals/quizQuestion.js
@@ -1,32 +1,35 @@
+const addEntryDetails = require("../code/addEntryDetails.js");
 const stringCompare = require("../code/stringCompare.js");
 
 module.exports = {
-    name: "quizQuestion",
+  name: "quizQuestion",
 
-    async execute(interaction, client) {
-        let newMessage = "";
-        try {
-            let quiz = client.quiz[interaction.user.id];
-            let answer = interaction.fields.getTextInputValue("question");
-            const threshold = 0.8;
-            let matchCorrect = quiz.answers.find((correctAnswer) => {
-              return (
-                stringCompare(correctAnswer.toLowerCase(), answer.toLowerCase()) >
-                threshold
-              );
-            });
-            if (matchCorrect) {
-              newMessage += "Correct!";
-            } else {
-              newMessage += `I'm sorry, but "${answer}" is not correct`;
-            }
-        } catch (e){
-            if (newMessage.length > 0) newMessage += "\n";
-            newMessage += `I'm having trouble with something. I got this error:\n   ${
-              e.name || "Error"
-            }: ${e.message}`;
-          } finally {
-            await interaction.reply({ content: newMessage });
-          }
+  async execute(interaction, client) {
+    let newMessage = "";
+    try {
+      let quiz = client.quiz[interaction.user.id];
+      let answer = interaction.fields.getTextInputValue("question");
+      const threshold = 0.8;
+      let matchCorrect = quiz.answers.find((correctAnswer) => {
+        return (
+          stringCompare(correctAnswer.toLowerCase(), answer.toLowerCase()) >
+          threshold
+        );
+      });
+      if (matchCorrect) {
+        newMessage += "Correct!";
+      } else {
+        newMessage += `I'm sorry, but "${answer}" is not correct`;
+      }
+
+      newMessage = addEntryDetails(quiz, newMessage, { slug: false });
+    } catch (e) {
+      if (newMessage.length > 0) newMessage += "\n";
+      newMessage += `I'm having trouble with something. I got this error:\n   ${
+        e.name || "Error"
+      }: ${e.message}`;
+    } finally {
+      await interaction.reply({ content: newMessage });
     }
-}
+  },
+};
