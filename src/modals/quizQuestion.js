bb97800a2c7772cc10763b1a8a419d4578d708f8
joewrotehaikus <78707288+joewrotehaikus@users.noreply.github.com>
2023-07-13 15:26:52 -0400

    Suppress link embed in quizQuestion.js

--- src/modals/quizQuestion.js
+++ src/modals/quizQuestion.js
@@ -29,7 +29,10 @@ module.exports = {
         e.name || "Error"
       }: ${e.message}`;
     } finally {
-      await interaction.reply({ content: newMessage });
+      await interaction.reply({
+        content: newMessage,
+        allowedMentions: { parse: [] },
+      });
     }
   },
 };
